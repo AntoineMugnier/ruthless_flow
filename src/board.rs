fc1914348191c623ad25194513f5c57934b38587
amugniere@gmail.com <antoine.mugnier@depixus.com>
2022-10-25 21:41:45 +0200

    try_moving_to_direction fn passed to head.rs

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::ops::Deref;
 use std::rc::Rc;
-use crate::heads::{Head, self};
+use crate::heads::{Head, self, HeadAction, HeadState};
 use crate::utils::{Direction, Coordinates};
 use crate::map::{Map, TileType};
 use std::sync::mpsc::{channel, Receiver};
@@ -39,45 +39,18 @@ pub fn new(events_receiver : Receiver<BoardEvevents>) -> Self{
     Self { map, heads, events_receiver, next_direction : Direction::None}
 }
 
-fn move_and_mark_tile(head : &mut HeadsCell, position: Coordinates, direction : Direction, map : &mut Map) -> Option<HeadsCell>{
-    let mut head = head.clone();
-    head.borrow_mut().set_provenance(direction);
-    map.set_tile(position, TileType::Marked);
-    head.borrow_mut().set_position(position);
-    Some(head.clone())
-}
 
-fn explore_direction(direction : Direction, head : &mut HeadsCell, map : &mut Map) -> Option<HeadsCell>{
-    let position = head.borrow().get_position();
-    if let Some((tile_type, position)) = map.get_tile(position, direction){
-        match tile_type {
-            TileType::Free =>{
-                Board::move_and_mark_tile(head, position,direction, map)
-            }
-            TileType::Marked => {
-                None
-            },
-            TileType::Separator => {
-                Board::move_and_mark_tile(head, position,direction, map)
-            },
-            TileType::Wall => {
-                Some(head.clone())
-            },
-        }
-    }
-    // TileType is out of range
-    else{
-        Some(head.clone())
-    }
-}
-fn split_heads_on_separator(&mut self){
-    for head in self.heads.iter(){
-        //self.heads.push(*head);
-    }
-}
+
 fn move_heads_handler(&mut self, direction : Direction){
     let map = &mut self.map;
-    let heads = self.heads.iter_mut().filter_map(|head| Board::explore_direction (direction, head, map)
+
+    self.heads = self.heads.iter().filter_map(|head| 
+        {
+           match head.borrow_mut().try_moving_to_direction(direction,map) {
+                 HeadState::TO_KEEP => return Some(head.clone()),
+                 HeadState::TO_KILL => return None,
+           }
+        }
 ).collect::<HeadsVec>();
 
 }
--- src/heads.rs
+++ src/heads.rs
@@ -1,9 +1,20 @@
 use crate::utils::{Direction, Coordinates};
-use crate::map::TileType;
+use crate::map::{TileType, Map};
 
 pub struct Head{
     position: Coordinates,
     coming_from : Direction
+}
+
+pub enum HeadAction {
+    HAS_NOT_MOVED,
+    HAS_MOVED,
+    MERGED,
+}
+
+pub enum HeadState {
+    TO_KILL,
+    TO_KEEP,
 }
 
 impl Head {
@@ -27,5 +38,58 @@ impl Head {
 
     pub fn get_provenance(&self) -> Direction{
         self.coming_from 
+    }
+
+    fn move_and_mark_tile(&mut self, position: Coordinates, direction : Direction, map : &mut Map) -> HeadAction{
+
+        self.coming_from = direction;
+        self.position = position;
+
+        map.set_tile(position, TileType::Marked);
+        HeadAction::HAS_MOVED
+    }
+/* 
+    fn split_heads_on_separator(&mut self){
+        for head in self.heads.iter(){
+            //self.heads.push(*head);
+        }
+    }
+*/
+    pub fn try_moving_to_direction(&mut self, direction : Direction, map : &mut Map) -> HeadState {
+        loop{
+            let status = self.explore_direction(direction, map);
+                match status{
+                    HeadAction::HAS_NOT_MOVED => {
+                        self.explore_direction(direction, map);
+                        return HeadState::TO_KEEP
+                    },
+                    HeadAction::HAS_MOVED => return HeadState::TO_KEEP,
+                    HeadAction::MERGED => return HeadState::TO_KILL,
+                }
+            }
+    }
+    
+    fn explore_direction(&mut self, direction : Direction, map : &mut Map) -> HeadAction{
+        let position = self.get_position();
+        if let Some((tile_type, position)) = map.get_tile(position, direction){
+            match tile_type {
+                TileType::Free =>{
+                    self.move_and_mark_tile(position, direction, map)
+                }
+                TileType::Marked => {
+                    HeadAction::MERGED
+                },
+                TileType::Separator => {
+                    self.move_and_mark_tile(position, direction, map)
+                },
+                TileType::Wall => {
+                    HeadAction::HAS_NOT_MOVED
+                },
+            }
+        }
+        // TileType is out of range
+        else{
+            HeadAction::HAS_NOT_MOVED
+        }
     }
 }
