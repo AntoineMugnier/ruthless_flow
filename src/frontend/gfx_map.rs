98a053758e6894d7662188d036da44f538cdf8ef
Antoine Mugnier <amugnier@europe.com>
2023-01-03 22:29:00 +0100

    gfx_map render method created

--- src/frontend/gfx_map.rs
+++ src/frontend/gfx_map.rs
@@ -1,4 +1,6 @@
 use std::collections::VecDeque;
+use piston_window::{Context, G2d, clear, rectangle};
+
 use crate::{utils::Coordinates, backend::{map::TileType}};
 
 pub struct GfxMap{
@@ -8,6 +10,12 @@ pub struct GfxMap{
 impl GfxMap{
     pub fn new(sto : VecDeque<Vec<TileType>>) -> GfxMap{
         GfxMap{sto}
+    }
+    pub fn render(&mut self, c: &Context, g: &mut G2d){
+        clear([1.0 ,1.0, 1.0 , 1.0], g);
+        rectangle([1.0, 0.0, 0.0, 1.0], // red
+                  [0.0, 0.0, 100.0, 100.0],
+                  c.transform, g);
     }
 
     pub fn add_line(&mut self, line:Vec<TileType>){
